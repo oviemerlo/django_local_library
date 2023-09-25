e8cd4136fe24ad6aeba339f8126039551ec3c9e9
Merlo Ovie <106752781+oviemerlo@users.noreply.github.com>
2023-09-24 22:07:20 -0400

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', "django-insecure-ixc^$ff-gx$t!n
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = ['web-production-7dba.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-7dba.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['*.railway.app']
 
 
 # Application definition
